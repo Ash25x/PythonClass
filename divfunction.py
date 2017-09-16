a7521ce30846b718cc3793d584deb5aa93475ed2
ash25x <ash122491@gmail.com>
2017-09-16 12:52:11 -0700

    all the work from class

--- divfunction.py
+++ divfunction.py
@@ -1,11 +1,14 @@
-from sys import argv
-
 
 def result(num1, num2):
 
-    if num1 > num2:
+    if (num1 > num2):
         div = num1 / num2
         print(f"This is your answer {div}.")
-    elif num1 < num2:
+    elif (num1 < num2):
         multi = num1 * 10
         print(f"This is your answer {multi} .")
+
+result(3,5)
+result(78,40)
+result(42,30)
+print("test")
---
+++ ex24.py
@@ -0,0 +1,6 @@
+#printing new lines using \
+
+#use """ to type a paragraph and assign it to the variable poem
+
+#print a bunch of --- to separate it
+#print the poem variable 
