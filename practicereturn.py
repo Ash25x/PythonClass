131971a6e0d7ca295f50d4453201a03b8102aaaf
ash25x <ash122491@gmail.com>
2017-09-19 22:14:48 -0700

    my pc files

---
+++ practicereturn.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    print(f"This is a {a} + b {b}.")
+    return a + b
+
+age = add(7,2)
+print(f"This is result {age} ")
+
+
+def subtract(x , y):
+    print(f"This is x {x} - y {y}")
+    answer = x + y
+    print(f"thiss is answer {answer}")
+
+sub = subtract(5, 2)
