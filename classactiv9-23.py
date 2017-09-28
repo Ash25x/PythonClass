3b02719e1086a13dc547b019c4f4163ba365b992
ash25x <ash122491@gmail.com>
2017-09-27 23:19:50 -0700

    hw#5

--- classactiv9-23.py
+++ classactiv9-23.py
@@ -1,6 +1,21 @@
+#1
+
+#ex34
+# animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+#
+# 1. bear
+# 2. python3.6
+# 3. bear
+# 4. peacock
+# 5. kangaroo
+# 6. python3.6
+# 7. whale
+# 8. kangaroo
+
+#2
 #create function and argument
 x = int(input("Enter the time in minutes you want to convert to seconds > "))
-#create a variable asking for input
+#create a variable asking for input, do math and print
 def sec (x):
     y= x * 60 + 42
     print(f"There are this many {y} seconds")
@@ -8,6 +23,7 @@ def sec (x):
 sec(x)
 #call function
 
+#3
 #create a variable that takes input
 kilo = int(input("Enter the amount of kilometers you want to convert: > "))
 def conv(kilo):
@@ -19,6 +35,8 @@ def conv(kilo):
 conv(kilo)
 #pull function
 
+
+#4
 faren = int(input("Enter the degrees in fahrenheight that you want to convert to celsius: > "))
 #assign variable to input integer
 def conv1(faren):
@@ -31,7 +49,7 @@ conv1(faren)
     #call function
 
 
-#4
+#5
 import math
 #import math library
 z = int(input("Enter one of the numbers 81, 19, 16, 121 to get the square root of it "))
@@ -51,6 +69,8 @@ elif z == 121:
 else:
     print("Wrong number mate! ")
 #make an else for other numbers they input
+
+#6
 #returns area of a circle r = 9
 r = int(input("Enter the radius you want to get the area of "))
 #make function and do the math math.pi represents pi ** is square
@@ -61,28 +81,27 @@ def area(r):
 area(r)
 
 
-#number 6
+#7
 #create input
 
 #define x in the strings
 
 #make if else statements if x in num6 then return true and print it otherwise false
-# num6 =str(input("Type a string in here >> "))
+num6 =str(input("Type a string in here >> "))
 #
-# def xinsid(num6):
-#
-#
-#     if ("x" in num6):
-#         print(True)
-#         return True
-#
-#     else:
-#         print(False)
-#         return False
-#
-# xinsid(num6)
+def xinsid(num6):
+    if ("x" in num6):
+         print(True)
+         return True
+
+    else:
+         print(False)
+         return False
+
+xinsid(num6)
 #call function
-#7
+
+#8
 #boolean function that returns true false if letters are a,e,i,o,u and in string by user
 num7 = input("Type something in here >")
 print(num7)
@@ -101,3 +120,114 @@ def aeiou(num7):
             return False
 
 aeiou(num7)
+
+#9 slack
+
+#write function
+
+def rad6(v):
+    r6 = (4/3) * math.pi*v**3
+    print(r6)
+
+rad6(5)
+
+#10
+#make function, take an input
+#make if/else statement
+#do math inside and print and return true false
+#call function
+w = int(input("Enter a number to divide by 3:  "))
+
+def num10(w):
+    if w % 3 == 0:
+        print(True)
+        return True
+
+    else:
+        print(False)
+        return False
+num10(w)
+
+#11
+#do import datetime to get preset function
+#assign a variable to datetie.date.today() and print it
+import datetime
+now = datetime.date.today()
+print (now)
+
+#12
+#do import datetime
+#assign variable to time formula and print variable
+import datetime
+current = datetime.datetime.now().time()
+print(current)
+
+#13
+#type up input, then function
+#use count and put string in quotes for it to count that string
+#asssign it  variable and print it
+g = input("Type in something here > ")
+
+def acount(g):
+    f = g.count('a')
+    print(f"{g} has an 'a' in it {f} times ")
+
+acount(g)
+
+
+
+#14
+#write an input and assign to variable
+#create function, have it print lenth of variable and call function
+
+e = input("Enter your word here >> ")
+
+def wrdcnt(e):
+    print(f"{e} has {len(e)} letters" )
+
+wrdcnt(e)
+
+#15
+#make function, leave it blank this time since we wont be taking input
+#start the count at 20, do a while loop and print what it counts
+#make count equal to itself minus 1 so it goes down each time loop goes
+#call fucntion
+
+def cnt():
+    count = 20
+    while count >= 0:
+        print(count)
+        count = count - 1
+
+cnt()
+
+#16
+#make it divisible by 2 remainder 0 after wrtiing function
+#print even if true, else make it odd and call function
+
+
+k =int(input("Enter a number here > "))
+def numodd(k):
+    if k % 2 == 0:
+        print("This is even")
+
+    else:
+        print("This is odd")
+
+numodd(k)
+
+
+#17
+
+# l = input("Enter a string here > ")
+# def strlng(l):
+#     if l == "": return 0
+
+
+# strlng(l)
+
+#18
+length = 0
+for char in myString:
+    length += 1
+print(length)
