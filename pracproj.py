682a8fc3e66f2a25042e5b87c265fbacb45dd2d3
ash25x <ash122491@gmail.com>
2017-10-11 20:47:51 -0700

    if else game

---
+++ pracproj.py
@@ -0,0 +1,7 @@
+def fight_start():
+	print("Alright it looks like it's time to fight let the games begin!")
+	for i in range(3, 1, -1):
+		print(i)
+		if i == 1:
+			print("WOOOOOO")
+	print("It looks like the crowd is roaring to see a fight. Let's give them a show")
