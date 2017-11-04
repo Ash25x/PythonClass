06fa50dcf2804bac6add9d8909f71c168e8ef416
ash25x <ash122491@gmail.com>
2017-11-04 05:49:11 -0700

    added images

--- flask_portfolio/portfolio/templates/__init__.py
+++ flask_portfolio/portfolio/templates/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "This is home"
+    return render_template("index.html")
 
-@app.route("/index/<name>")
-def profile(name):
-    return render_template("index.html", name=name)
+@app.route("/profile.html")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/annoying.html")
+def annoying():
+    return render_template("annoying.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
